35619fea2cf01bbeddd206ad1de9f6b876b84637
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-05 04:08:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(file_path, emoji)
   emoji = library.key.find do |key|
     library[key][:english] == emoji
   end
-  japan_emoji ? library[emoji][:japanese] : "Sorru sir."
+  emoji ? library[emoji][:japanese] : "Sorru sir."
 end
 
 def get_english_meaning(file_path, japan_emoji)
