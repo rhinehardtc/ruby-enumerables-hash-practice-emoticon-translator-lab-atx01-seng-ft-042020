628f7ccaa036fafcfe07bbb310cc67fe137b070e
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-05 04:00:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,11 +13,13 @@ def load_library(file_path)
   container
 end
 
-def get_japanese_emoticon(english_emoji)
+def get_japanese_emoticon(file_path, english_emoji)
   library = load_library(file_path)
+  container.each do |index|
+    japan_emoji = 
   
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, japan_emoji)
+  library = load_library(file_path)
 end
