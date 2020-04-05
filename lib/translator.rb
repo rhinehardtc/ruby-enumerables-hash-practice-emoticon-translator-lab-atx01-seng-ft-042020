394b049e3b5725ab71d91d3e990d9405046060d2
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-05 03:44:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ def load_library(file_path)
   emojis = YAML.load_file(file_path)
   container = {}
   
-  emojis.each do |name,face|
+  emojis.each do |name, face|
     container[name] = {}
     container[name][:english] = face[0]
-    container[name]{:japanese] = face[1]
+    container[name][:japanese] = face[1]
   end
   container
 end
